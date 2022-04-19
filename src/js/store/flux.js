7b3a990f55f61989449f7d7c15b090dd70794ba3
danyez1 <cdanielyanez144@gmail.com>
2022-04-19 00:22:19 +0000

    second

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,43 +1,46 @@
+import { ContactList } from "../component/ContacList";
+
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
-		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+		store: { c
+			ContactList:[]
+		
 		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+		  actions: {
+			  getContact:()=> {
+				var requestOptions = {
+					method: "GET",
+					redirect: "follow",
+				};
+		
+				fetch("https://assets.breatheco.de/apis/fake/todos/user/cruzdanielyanez",
+					requestOptions)
+					.then((response) => response.json())
+					.then((result) => setTodolist(result))
+					.catch((error) => console.log("error", error));		
+			}	
+			addContact:(Contact)=>{
+				fetch("https://assets.breatheco.de/apis/fake/todos/user/cruzdanielyanez",{
+					method: "GET",
+					redirect: "follow",
+					headers: {
+						"Content-Type":"application/json",
+					},
+					body: JSON.stringify([...getStore().ContactList,
+					{LABEL:Contact,donde:false}])
+ 
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+				}
+				
+				
 
-				//reset the global store
-				setStore({ demo: demo });
 			}
+			
+
+
+
+
+
 		}
 	};
 };
